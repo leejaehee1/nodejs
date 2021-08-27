2192ad22c4c16b4c88370b6b5994aae7819585e9
aaakch0316 <aaakch0316@gmail.com>
2021-08-27 10:45:35 +0900

    FE-BE [+] Connect PunchListPage for testing

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,7 @@ const {User} = require('../models');
 
 // testimport DB // king
 const { authority } = require('../models');
+const { PunchList } = require('../models');
 
 
 router.use(bodyParser.urlencoded({extended: false}));
@@ -114,17 +115,52 @@ router.post('/delete', async (req, res, next) => {
 // 따로 분리된 .js 파일을 만들어야함.
 
 router.get('/authority', (req, res) => {
-    console.log(req.body)
-    console.log("들어온다")
-    // authority.findOne({ where : { authName : 'QC'}
-    // })
     authority.findAll({
-        attributes: ['authority', 'authName', 'remarks']
+        attributes: [ 'authority', 'authName', 'remarks']
+        // where: { id: [1]}
     })
     .then(result => {
-        res.json({result, test: "test", error: null})
+        // res.json({"data":result, test: "test", error: null})
+        console.log(result.body)
+        res.json(result)
     })
+    .catch(err => {
+        console.error(err);
+        res.json({error: null}
+    )});
 })
+
+
+router.get('/list', (req, res) => {
+    const queyRangeString = req.query.range
+    const startSetString = queyRangeString.indexOf('[')
+    const midSetString = queyRangeString.indexOf(',')
+    const endSetString = queyRangeString.indexOf(']')
+    const offset = Number(queyRangeString.slice(startSetString+1, midSetString))
+    const limit = Number(queyRangeString.slice(midSetString+1, endSetString))
+    console.log(offset)
+    console.log(limit)
+    PunchList.findAll({
+        PunchList: [ `projectID`, `punchID`, `category`, `system`, `subsystem`, `discipline`, `status`, `unit`, `area`, 
+        `tagNumber`, `bulkItem`, `bulkName`, `department`, `targetDate`, `issuedDate`, `issuedBy`, `raisedBy`, `completedDate`, 
+        `completedBy`, `confirmedDate`, `confirmedBy`, `closedDate`, `closedBy`, `scheduleKey`, `scheStartDate`, `scheFinishDate`,
+         `designChgReq`, `materialReq`, `issueDescription`, `completeComment`, `notAcceptComment`, `difficulty`, `scheduleImpact`, 
+         `costImpact`, `keyword2`, `keyword3`, `keyword4`, `drawingNo`, `awpCode`],
+         offset: offset,
+         limit: limit,
+    })
+    .then(result => {
+        // res.header("Content-Range", `getProducts 0-4/${result.length}`);
+        res.set('Content-Range', `getProducts 0-${result.length}/${result.length}`)
+        res.set('Access-Control-Expose-Headers', 'Content-Range')
+        res.send(result);
+    })
+    .catch(err => {
+        res.json({error: null}
+    )});
+})
+
+
 
 
 module.exports = router;
