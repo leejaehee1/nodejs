76b878fcf7dd6b6bbceb21becc4563c37962fef3
aaakch0316 <aaakch0316@gmail.com>
2021-10-08 13:00:34 +0900

    update hexacon01 test

--- routes/summury.js
+++ routes/summury.js
@@ -36,7 +36,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host:'127.0.0.1',
     user: 'root',
-    password:'root',
+    password:'hexacon01',
     database: 'punch',
 });
 
